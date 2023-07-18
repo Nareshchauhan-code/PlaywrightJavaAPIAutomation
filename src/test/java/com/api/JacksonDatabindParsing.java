79865702442a270b62cbab7c3b3c742ff3095227
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-18 18:31:22 +0530

    API

--- src/test/java/com/api/JacksonDatabindParsing.java
+++ src/test/java/com/api/JacksonDatabindParsing.java
@@ -1,5 +1,7 @@
 package com.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.playwright.APIRequest;
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
@@ -7,21 +9,31 @@ import com.microsoft.playwright.Playwright;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GETAPICall {
+import java.io.IOException;
+
+public class JacksonDatabindParsing {
 
     @Test
-    void getAPICall() {
+    void getAPICall() throws IOException {
 
         Playwright playwright = Playwright.create();
         APIRequest apiRequest = playwright.request();
         APIRequestContext apiRequestContext = apiRequest.newContext();
         APIResponse apiResponse = apiRequestContext.get("https://gorest.co.in/public/v2/users");
-        // Assertion
+
+        // Verify the status code and assert with Testng
         int statusCode = apiResponse.status();
         System.out.println(statusCode);
-        Assert.assertEquals(statusCode,200);
-        System.out.println(apiResponse.toString());
-        System.out.println(apiResponse.text());
+        Assert.assertEquals(statusCode, 200);
+        System.out.println(apiResponse.statusText());
+
+        // Parsing Json Body
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(apiResponse.body());
+
+        System.out.println(jsonNode.toPrettyString());
+
 
     }
 }
